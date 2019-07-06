97ab8ee0d10e7a617464990c71690f40fe56b2e1
Khalid Elatawy <khalidel-atawy@khalids-imac.local>
2019-07-06 13:00:44 +0200

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Khalid Saleh Elatawy on 7/6/19.
+//  Copyright © 2019 Khalid Saleh Elatawy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
