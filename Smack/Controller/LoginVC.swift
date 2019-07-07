dc991746db873a0ec7ca96de76d50133570108ba
Khalid Elatawy <khalidel-atawy@khalids-imac.local>
2019-07-07 14:03:09 +0200

    Finish Login UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Khalid Saleh Elatawy on 7/7/19.
+//  Copyright © 2019 Khalid Saleh Elatawy. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Khalid Saleh Elatawy on 7/6/19.
+//  Copyright © 2019 Khalid Saleh Elatawy. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
