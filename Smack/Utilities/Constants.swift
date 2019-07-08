cc4c612bcd5c5026109b12c115caff4182daeb72
Khalid Elatawy <khalidel-atawy@khalids-imac.local>
2019-07-08 12:49:36 +0200

    create the AuthService and the Auth variables

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,8 +8,19 @@
 
 import Foundation
 
-//Segues
+typealias CompletionHandler = (_ Success : Bool) -> ()
 
+//URL constants
+let BASE_URL = "https://chattychatjb.herokuapp.com/v1"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
+//Segues
 let TO_LOGIN = "toLogin"
 let TO_CREAT_ACCOUNT = "toCreatAccount"
 let UNWIND = "unwindToChannel"
+
+//user defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
